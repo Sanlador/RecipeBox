647d17394554058578fed6a4e04b52689a2aa2a9
KuanLunTseng <43557071+KuanLunTseng@users.noreply.github.com>
2019-10-15 10:36:05 -0700

    Update MainActivity.java

--- src/main/java/com/kuanluntseng/show/MainActivity.java
+++ src/main/java/com/kuanluntseng/show/MainActivity.java
@@ -16,9 +16,11 @@ import java.sql.*;
 import java.util.*;
 
 public class MainActivity extends AppCompatActivity {
+    
+    // define widgets as global variables
     TextView textView;
     Button button;
-
+    // storing strings for data
     String result;
 
     @Override
@@ -26,50 +28,69 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Declare button
+        // binding with id
          button = (Button) findViewById(R.id.button);
          textView = findViewById(R.id.textView);
 
+        //declare a button listener to check whether the button is clicked
         button.setOnClickListener(new View.OnClickListener() {
+            
+            // event for button clicking
             @Override
             public void onClick(View view) {
+                
+                // create a thread if botton got clicked and start it
                 Thread thread = new Thread(mutiThread);
                 thread.start();
             }
         });
     }
 
-    /*
-
-    */
-
+    // the purpose of creating threads instead of doing it in 
+    // MainActivity() prevents entire function get jammed if 
+    // users take a too long time to build up the connection.
     private Runnable mutiThread = new Runnable() {
         @Override
         public void run() {
             try {
+                // local database
                 URL url = new URL("http://10.0.0.87/GetData.php");
+                
                 HttpURLConnection connection = (HttpURLConnection) url.openConnection();
                 connection.setRequestMethod("POST");
-                connection.setDoOutput(true);
-                connection.setDoInput(true);
-                connection.setUseCaches(false);
-                connection.connect();
-
+                connection.setDoOutput(true);   // allow output
+                connection.setDoInput(true);    // allow input
+                connection.setUseCaches(false); // allow no caches
+                connection.connect();           // building connection
+                
+                // respondse from the connection
                 int responseCode = connection.getResponseCode();
+                
+                // if there is no error from response 
                 if(responseCode == HttpURLConnection.HTTP_OK) {
+                    // get input strings
                     InputStream inputStream = connection.getInputStream();
+                    // load the strings
                     BufferedReader bufReader = new BufferedReader(new InputStreamReader(inputStream, "utf-8"), 8);
+                    // where to put strings
                     String box = "";
+                    // where to load strings
                     String line = null;
+                    // append strings if there are string it reads
                     while ((line = bufReader.readLine()) != null) {
                         box += line + "\n";
                     }
+                    // close the stream
                     inputStream.close();
+                    // store it into global vairable
                     result = box;
                 }
             } catch(Exception e) {
+                    // report if something happened
                     result = e.toString();
             }
+            
+            // whenever a thread has done then change the text in textview
             runOnUiThread(new Runnable() {
                 @Override
                 public void run() {
