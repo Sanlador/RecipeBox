90c445d6e9cc3657817871fa9bf50f3b97c6186a
KuanLunTseng <43557071+KuanLunTseng@users.noreply.github.com>
2019-10-16 11:25:01 -0700

    Add files via upload

---
+++ app/src/main/java/CS561/recipebox/Contact.java
@@ -0,0 +1,35 @@
+package com.kuanluntseng.recyclerview_q;
+
+import java.util.ArrayList;
+
+// Struct of Contact
+public class Contact {
+    private String mName;
+    private boolean mOnline;
+
+    public Contact(String name, boolean online) {
+        mName = name;
+        mOnline = online;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public boolean isOnline() {
+        return mOnline;
+    }
+
+    private static int lastContactId = 0;
+
+    // Use class Contact to create an list of information
+    public static ArrayList<Contact> createContactsList(int numContacts) {
+        ArrayList<Contact> contacts = new ArrayList<Contact>();
+
+        for (int i = 1; i <= numContacts; i++) {
+            contacts.add(new Contact("Person " + ++lastContactId, i <= numContacts / 2));
+        }
+
+        return contacts;
+    }
+}
