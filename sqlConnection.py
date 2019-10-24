7bccec83efdb16d7c34a4079a70753f219a159a1
Ali Karimyar <ali.karimyar@gmail.com>
2019-10-24 12:24:55 -0700

    Delete sqlConnection.py

--- sqlConnection.py
+++
@@ -1,8 +0,0 @@
-import pyodbc
-
-conn = pyodbc.connect('Driver={SQL Server};Server=tcp:recipebox01.database.windows.net,1433;Database=RecipeDB;Uid=recipeOSU@recipebox01;Pwd=recipe32!;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;')
-cursor = conn.cursor()
-#PUT QUERIES HERE
-cursor.execute("Select * from Test")
-for row in cursor.fetchall():
-	print(row)
